6ad419e484d1448246bb67d1b70fc76bfcab45f4
Mp97871431 <carlos.rocha9787@gmail.com>
2025-02-06 16:06:59 -0300

    axercicio

---
+++ cnaula14/aula.js
@@ -0,0 +1,16 @@
+/*aula de objetos em javascript*/
+
+var funcionario = {
+    'nome': 'pedro souza gomes',
+    'ano de nacimento': '1972',
+    'cpf': '111.111.111.11',
+    'cargo': 'analista de dados',
+}
+
+/*se eu quiser selecionar um elemento, diferentemente dos arrays usamos ['']*/
+
+console.log(funcionario ['nome']); 
+
+/*ou usamos . e colocamos o nome do elemento*/
+
+console.log(funcionario.cpf);
