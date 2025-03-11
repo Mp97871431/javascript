e0af3617088bd6d790c9437a97a0adfd13160755
Mp97871431 <carlos.rocha9787@gmail.com>
2025-03-11 14:40:33 -0300

    exercucios

---
+++ cnaula25/script.js
@@ -0,0 +1,10 @@
+
+var nu1 = document.getElementById('n1')
+var res = document.getElementById('res')
+
+
+function verificar(){
+    if( nu1 > 65) {
+        res.innerHTML = 'ingresso gratis'
+    } 
+}
